1528e75622ad19d102268b8773682fc99f03e5e1
Liam Defty <liam@bigbite.net>
2020-04-09 14:12:38 +0100

    0.2.1 Add the ability to generate comments

--- plugin/Seeder/Generator.php
+++ plugin/Seeder/Generator.php
@@ -3,12 +3,26 @@
 namespace WP_Cypress\Seeder;
 
 use WP_Cypress\Seeder\Seeds\Post;
+use WP_Cypress\Seeder\Seeds\Comment;
 
 class Generator {
 	public function posts( $properties = [], $count = 1 ) {
 		$progress = \WP_CLI\Utils\make_progress_bar( 'Generating posts', $count );
 
 		$post = new Post( $properties );
+
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post->generate();
+			$progress->tick();
+		}
+
+		$progress->finish();
+	}
+
+	public function comments( $properties = [], $count = 1 ) {
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Generating comments', $count );
+
+		$post = new Comment( $properties );
 
 		for ( $i = 1; $i <= $count; $i++ ) {
 			$post->generate();
--- plugin/Seeder/bootstrap.php
+++ plugin/Seeder/bootstrap.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/Seeds/SeedInterface.php';
 require_once __DIR__ . '/Traits/Date.php';
 require_once __DIR__ . '/Seeds/Seed.php';
 require_once __DIR__ . '/Seeds/Post.php';
+require_once __DIR__ . '/Seeds/Comment.php';
 require_once __DIR__ . '/Generator.php';
 require_once __DIR__ . '/SeederInterface.php';
 require_once __DIR__ . '/Seeder.php';
