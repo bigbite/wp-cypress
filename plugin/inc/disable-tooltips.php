85a1671db454d050a6b74f563f540d7a75f6d3ad
tfrommen <info@tfrommen.de>
2020-05-26 10:06:37 +0200

    Use __DIR__

--- plugin/inc/disable-tooltips.php
+++ plugin/inc/disable-tooltips.php
@@ -4,7 +4,7 @@ function disable_tooltips() {
 	add_action( 'enqueue_block_editor_assets', function () {
 		wp_enqueue_script(
 			'wp-cypress-disable-tooltips',
-			plugins_url( '/assets/disable-tooltips.js', dirname( __FILE__ ) ),
+			plugins_url( '/assets/disable-tooltips.js', __DIR__ ),
 			[ 'wp-blocks' ],
 			filemtime( WP_CYPRESS_PLUGIN . '/assets/disable-tooltips.js' ),
 			false
