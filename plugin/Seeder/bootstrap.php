5389beccfc22c9883589379f1627ec35fef15ab9
tfrommen <info@tfrommen.de>
2020-06-08 13:39:42 +0200

    Use Composer for autoloading

--- plugin/Seeder/bootstrap.php
+++ plugin/Seeder/bootstrap.php
@@ -11,13 +11,5 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 }
 
 require_once __DIR__ . '/utils.php';
-require_once __DIR__ . '/Seeds/SeedInterface.php';
-require_once __DIR__ . '/Seeds/Seed.php';
-require_once __DIR__ . '/Seeds/Post.php';
-require_once __DIR__ . '/Seeds/Comment.php';
-require_once __DIR__ . '/Generator.php';
-require_once __DIR__ . '/SeederInterface.php';
-require_once __DIR__ . '/Seeder.php';
-require_once __DIR__ . '/Command.php';
 
 WP_CLI::add_command( 'seed', Command::class );
