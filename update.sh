aba30317503c5069e745d40755f8d9f38edb58ae
Jason Agnew <j@agnew.co>
2020-10-14 10:09:32 +0100

    Fix for soft reset to include versions

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 version=$1
+HARD_RESET=$2
 
 shopt -s extglob
-rm -rf !(seeds|wp-content|update.sh|.htaccess)
+rm -rf !(wp-config.php|seeds|wp-content|update.sh|.htaccess)
+
+if ${HARD_RESET}; then rm wp-config.php && echo "Hello"; fi
+
 cp -rfp ../${version}/* ./
 
 wp --allow-root core version
