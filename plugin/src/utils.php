665c34c37029c01cac3e258d6704cdb75a21f576
Jason Agnew <j@agnew.co>
2020-10-15 12:26:18 +0100

    Handle auth switching correctly

--- plugin/src/utils.php
+++ plugin/src/utils.php
@@ -10,3 +10,12 @@ namespace WP_Cypress\Utils;
 function now(): string {
 	return current_time( 'Y-m-d H:i:s' );
 }
+
+
+if ( class_exists( 'WP_CLI' ) ) {
+	$wp_cypress_set_user = function( $args, $assoc_args ) {
+		file_put_contents( get_home_path() . '.userid', $args[0] );
+		\WP_CLI::success( 'Current User set to ID: ' . $args[0] );
+	};
+	\WP_CLI::add_command( 'wp-cypress-set-user', $wp_cypress_set_user );
+}
