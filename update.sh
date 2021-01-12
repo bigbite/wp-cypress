8da8ea8b748abcfa5ac54705ae4ade00a79b4132
Jason Agnew <j@agnew.co>
2021-01-12 11:16:41 +0000

    Only reset images on hard reset otherwise seed images would be lost before first test run

--- update.sh
+++ update.sh
@@ -5,9 +5,7 @@ shopt -s extglob
 rm -rf !(wp-config.php|wp-cypress-config.php|seeds|wp-content|update.sh|.htaccess)
 
 # Ensure uploads work
-rm -rf /var/www/html/wp-content/uploads
-mkdir -p /var/www/html/wp-content/uploads
-chmod -R 777 /var/www/html/wp-content/uploads
+if ${HARD_RESET}; then rm -rf /var/www/html/wp-content/uploads && mkdir -p /var/www/html/wp-content/uploads && chmod -R 777 /var/www/html/wp-content/uploads; fi
 
 if ${HARD_RESET}; then rm wp-config.php; fi
 
